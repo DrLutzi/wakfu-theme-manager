fe25fb2132843c006efb401069695afe256f375c
Nicolas Lutz <nicolas.lutz@usherbrooke.ca>
2023-10-08 18:12:31 -0400

    Added a comment to specify the usage of the deploy script.

--- windeploy.bat
+++ windeploy.bat
@@ -1,4 +1,5 @@
 REM requires windeployqt.exe to be in PATH
+REM first arg is the source folder, second arg is destination folder
 windeployqt.exe %2\wtm.exe
 set translationFile=%1\ts\wtm_fr-FR.qm
 set translationDest=%2\wtm_fr-FR.qm
